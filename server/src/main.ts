5be34f03900649a53b5d3d158544015c408ff7af
alzh0817 <aleksandr.zheleznov@netcracker.com>
2022-08-06 22:11:53 +0400

    Backend was initialized

---
+++ server/src/main.ts
@@ -0,0 +1,17 @@
+import {NestFactory} from "@nestjs/core";
+import {AppModule} from "./app.module";
+
+
+const start = async () => {
+    try {
+        const server = await NestFactory.create(AppModule);
+        const PORT = process.env.port || 5000;
+        await server.listen(PORT, () => {
+            console.log(`Server runs on port: ${PORT}`);
+        });
+    } catch (e) {
+        console.error("Something went wrong with server starting...: " + e)
+    }
+}
+
+start();
