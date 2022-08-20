daea21d620bd7411dc330d9ce87ab2f6e9834f72
alzh0817 <aleksandr.zheleznov@netcracker.com>
2022-08-20 19:13:14 +0400

    SEO optimization, track searching, doc update

--- client/layouts/MainLayout.tsx
+++ client/layouts/MainLayout.tsx
@@ -2,14 +2,31 @@ import React, {FC} from 'react';
 import Navbar from "../components/Navbar";
 import {Container} from "@mui/material";
 import Player from "../components/Player";
+import Head from "next/head";
 
-type Props = {
+type MainLayoutProps = {
+    title?: string;
+    description?: string;
+    keywords?: string;
     children?: React.ReactNode
 };
 
-const MainLayout: FC<Props> = ({children}) => {
+const MainLayout: FC<MainLayoutProps> = ({
+                                             title,
+                                             description = "",
+                                             keywords,
+                                             children
+                                         }) => {
     return (
         <>
+            <Head>
+                <title>{title || "Music platform"}</title>
+                <meta name="description"
+                      content={"Music platform - bet place in the world for listen to music! " + description}/>
+                <meta name="robots" content="index, follow"/>
+                <meta name="keywords" content={"Music,tracks," + keywords}/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            </Head>
             <Navbar/>
             <Container style={{margin: '90px auto'}}>
                 {children}
